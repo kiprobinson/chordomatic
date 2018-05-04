111ef032bc9191d82592790c5feea65fa19389c4
kiprobinson <kip.robinson@gmail.com>
2018-05-04 00:46:50 -0400

    now we have a working guitar that makes music!

--- app/chord-player.js
+++ app/chord-player.js
@@ -1,7 +1,10 @@
 "use strict";
 
+//audio map generated from spreadsheeet. for any given pitch, lists all potential audio files that play it.
+const AUDIO_MAP = {"B0":[{"fret":0,"file":"B0/00"}],"C1":[{"fret":1,"file":"B0/01"}],"C#1":[{"fret":2,"file":"B0/02"}],"D1":[{"fret":3,"file":"B0/03"}],"D#1":[{"fret":4,"file":"B0/04"}],"E1":[{"fret":0,"file":"E1/00"}],"F1":[{"fret":1,"file":"E1/01"}],"F#1":[{"fret":2,"file":"E1/02"}],"G1":[{"fret":3,"file":"E1/03"}],"G#1":[{"fret":4,"file":"E1/04"}],"A1":[{"fret":5,"file":"E1/05"}],"A#1":[{"fret":6,"file":"E1/06"}],"B1":[{"fret":0,"file":"B1/00"}],"C2":[{"fret":1,"file":"B1/01"}],"C#2":[{"fret":2,"file":"B1/02"}],"D2":[{"fret":3,"file":"B1/03"}],"D#2":[{"fret":4,"file":"B1/04"}],"E2":[{"fret":5,"file":"B1/05"},{"fret":0,"file":"E2/00"}],"F2":[{"fret":6,"file":"B1/06"},{"fret":1,"file":"E2/01"}],"F#2":[{"fret":7,"file":"B1/07"},{"fret":2,"file":"E2/02"}],"G2":[{"fret":8,"file":"B1/08"},{"fret":3,"file":"E2/03"}],"G#2":[{"fret":9,"file":"B1/09"},{"fret":4,"file":"E2/04"}],"A2":[{"fret":10,"file":"B1/10"},{"fret":5,"file":"E2/05"},{"fret":0,"file":"A2/00"}],"A#2":[{"fret":11,"file":"B1/11"},{"fret":6,"file":"E2/06"},{"fret":1,"file":"A2/01"}],"B2":[{"fret":12,"file":"B1/12"},{"fret":7,"file":"E2/07"},{"fret":2,"file":"A2/02"}],"C3":[{"fret":13,"file":"B1/13"},{"fret":8,"file":"E2/08"},{"fret":3,"file":"A2/03"}],"C#3":[{"fret":14,"file":"B1/14"},{"fret":9,"file":"E2/09"},{"fret":4,"file":"A2/04"}],"D3":[{"fret":15,"file":"B1/15"},{"fret":10,"file":"E2/10"},{"fret":5,"file":"A2/05"},{"fret":0,"file":"D3/00"}],"D#3":[{"fret":16,"file":"B1/16"},{"fret":11,"file":"E2/11"},{"fret":6,"file":"A2/06"},{"fret":1,"file":"D3/01"}],"E3":[{"fret":17,"file":"B1/17"},{"fret":12,"file":"E2/12"},{"fret":7,"file":"A2/07"},{"fret":2,"file":"D3/02"}],"F3":[{"fret":13,"file":"E2/13"},{"fret":8,"file":"A2/08"},{"fret":3,"file":"D3/03"}],"F#3":[{"fret":14,"file":"E2/14"},{"fret":9,"file":"A2/09"},{"fret":4,"file":"D3/04"}],"G3":[{"fret":15,"file":"E2/15"},{"fret":10,"file":"A2/10"},{"fret":5,"file":"D3/05"},{"fret":0,"file":"G3/00"}],"G#3":[{"fret":16,"file":"E2/16"},{"fret":11,"file":"A2/11"},{"fret":6,"file":"D3/06"},{"fret":1,"file":"G3/01"}],"A3":[{"fret":17,"file":"E2/17"},{"fret":12,"file":"A2/12"},{"fret":7,"file":"D3/07"},{"fret":2,"file":"G3/02"}],"A#3":[{"fret":13,"file":"A2/13"},{"fret":8,"file":"D3/08"},{"fret":3,"file":"G3/03"}],"B3":[{"fret":14,"file":"A2/14"},{"fret":9,"file":"D3/09"},{"fret":4,"file":"G3/04"},{"fret":0,"file":"B3/00"}],"C4":[{"fret":15,"file":"A2/15"},{"fret":10,"file":"D3/10"},{"fret":5,"file":"G3/05"},{"fret":1,"file":"B3/01"}],"C#4":[{"fret":16,"file":"A2/16"},{"fret":11,"file":"D3/11"},{"fret":6,"file":"G3/06"},{"fret":2,"file":"B3/02"}],"D4":[{"fret":17,"file":"A2/17"},{"fret":12,"file":"D3/12"},{"fret":7,"file":"G3/07"},{"fret":3,"file":"B3/03"}],"D#4":[{"fret":13,"file":"D3/13"},{"fret":8,"file":"G3/08"},{"fret":4,"file":"B3/04"}],"E4":[{"fret":14,"file":"D3/14"},{"fret":9,"file":"G3/09"},{"fret":5,"file":"B3/05"},{"fret":0,"file":"E4/00"}],"F4":[{"fret":15,"file":"D3/15"},{"fret":10,"file":"G3/10"},{"fret":6,"file":"B3/06"},{"fret":1,"file":"E4/01"}],"F#4":[{"fret":16,"file":"D3/16"},{"fret":11,"file":"G3/11"},{"fret":7,"file":"B3/07"},{"fret":2,"file":"E4/02"}],"G4":[{"fret":17,"file":"D3/17"},{"fret":12,"file":"G3/12"},{"fret":8,"file":"B3/08"},{"fret":3,"file":"E4/03"}],"G#4":[{"fret":13,"file":"G3/13"},{"fret":9,"file":"B3/09"},{"fret":4,"file":"E4/04"}],"A4":[{"fret":14,"file":"G3/14"},{"fret":10,"file":"B3/10"},{"fret":5,"file":"E4/05"}],"A#4":[{"fret":15,"file":"G3/15"},{"fret":11,"file":"B3/11"},{"fret":6,"file":"E4/06"}],"B4":[{"fret":16,"file":"G3/16"},{"fret":12,"file":"B3/12"},{"fret":7,"file":"E4/07"}],"C5":[{"fret":17,"file":"G3/17"},{"fret":13,"file":"B3/13"},{"fret":8,"file":"E4/08"}],"C#5":[{"fret":14,"file":"B3/14"},{"fret":9,"file":"E4/09"}],"D5":[{"fret":15,"file":"B3/15"},{"fret":10,"file":"E4/10"}],"D#5":[{"fret":16,"file":"B3/16"},{"fret":11,"file":"E4/11"}],"E5":[{"fret":17,"file":"B3/17"},{"fret":12,"file":"E4/12"}],"F5":[{"fret":13,"file":"E4/13"}],"F#5":[{"fret":14,"file":"E4/14"}],"G5":[{"fret":15,"file":"E4/15"}],"G#5":[{"fret":16,"file":"E4/16"}],"A5":[{"fret":17,"file":"E4/17"}]};
+
 //state-  string tunings, net pitch alter, selected frets
-var ChordPlayer = {
+let ChordPlayer = {
   state: {
     strings: 'E2 A2 D3 G3 B3 E4'.split(/ /).map(s => new Pitch(s)),
     transpose: 0,
@@ -11,39 +14,31 @@ var ChordPlayer = {
   },
   
   init() {
-    var $guitar = $('#guitar');
-    $guitar.on('click', '.note', function() {
-      var $this = $(this);
-      var stringId = $this.data('string');
-      var setActive = !$this.hasClass('active');
-      var filterd = $guitar.find('.note.active').filter(function() {return $(this).data('string') === stringId});
-      $guitar.find('.note.active').filter(function() {return $(this).data('string') === stringId}).removeClass('active');
-      if(setActive) {
-        $this.addClass('active');
-        ChordPlayer.state.frets[stringId] = $this.parent().data('fret');
-      }
-      else {
-        ChordPlayer.state.frets[stringId] = null;
-      }
+    let $guitar = $('#guitar');
+    $guitar.on('click', '.note', ChordPlayer.handleNoteClick);
+    
+    $('#test').on('click', function() {
+      //debugger;
+      ChordPlayer.playChord();
     });
     
     ChordPlayer.draw();
   },
   
   draw() {
-    var $guitar = $('#guitar');
+    let $guitar = $('#guitar');
+    let numStrings = ChordPlayer.state.strings.length;
     $guitar.find('#headstock, #fretboard .fret').each(function() {
-      var $fret = $(this);
-      var fretId = $fret.data('fret');
-      var numStrings = ChordPlayer.state.strings.length;
+      let $fret = $(this);
+      let fretId = $fret.data('fret');
       
       $fret.empty();
-      for(var i = 0; i < ChordPlayer.state.strings.length; i++) {
-        var pitch = ChordPlayer.state.strings[i].transpose(fretId);
-        var pitchName = pitch.getName();
-        var noteName = pitch.note.getName(ChordPlayer.state.useFlats);
+      for(let i = 0; i < numStrings; i++) {
+        let pitch = ChordPlayer.state.strings[i].transpose(fretId);
+        let pitchName = pitch.getName();
+        let noteName = pitch.note.getName(ChordPlayer.state.useFlats);
         
-        var $note = $('<div/>').addClass('note').css({width: `calc(100% / ${numStrings})`, left: `calc(${i} * (100% / ${numStrings}))`});
+        let $note = $('<div/>').addClass('note').css({width: `calc(100% / ${numStrings})`, left: `calc(${i} * (100% / ${numStrings}))`});
         $note.data({note: pitchName, string: i});
         //$note.attr({'data-note': pitchName, 'data-string': i});
         if(ChordPlayer.state.frets[i] === fretId)
@@ -53,18 +48,139 @@ var ChordPlayer = {
         $fret.append($note);
       }
     });
+    
+    const SVG = '<svg baseProfile="full" viewBox="0 0 20 1000" preserveAspectRatio="none">'
+              + '  <path d="M10 0 C 10 500, 10 500, 10 1000" stroke="olive" fill="transparent" />'
+              + '</svg>'
+    ;
+    let $strings = $guitar.find('#strings');
+    $strings.empty();
+    for(let i = 0; i < numStrings; i++) {
+      //two strings- one for the part above the fingered fret (which won't vibrate), one for the part below (which will)
+      let staticStringHeight = ChordPlayer.getStaticStringHeight(i);
+      
+      let $staticString = $('<div/>').addClass(`string static string-${i}`).data('stringId', i);
+      $staticString.css({
+        width: `calc(100% / ${numStrings})`,
+        left: `calc(${i} * (100% / ${numStrings}))`,
+        top: 0,
+        height: `${staticStringHeight}%`
+      });
+      $staticString.html(SVG);
+      $strings.append($staticString);
+      
+      let $activeString = $('<div/>').addClass(`string active string-${i}`).data('stringId', i);
+      $activeString.css({
+        width: `calc(100% / ${numStrings})`,
+        left: `calc(${i} * (100% / ${numStrings}))`,
+        top: `${staticStringHeight}%`,
+        height: `${(100-staticStringHeight)}%`
+      });
+      $activeString.html(SVG);
+      $strings.append($activeString);
+    }
   },
   
-  play(string) {
-    var el = $(`#string-${string}`)[0];
-    if(el)
-      el.play();
+  handleNoteClick() {
+    let $note = $(this);
+    let $guitar = $('#guitar');
+    let stringId = $note.data('string');
+    let setActive = !$note.hasClass('active');
+    $guitar.find('.note.active').filter(function() {return $(this).data('string') === stringId}).removeClass('active');
+    if(setActive) {
+      $note.addClass('active');
+      ChordPlayer.state.frets[stringId] = $note.parent().data('fret');
+    }
+    else {
+      ChordPlayer.state.frets[stringId] = null;
+    }
+    
+    ChordPlayer.setAudio(stringId);
+    
+    //update the height of the active/inactive portion of this string
+    let staticStringHeight = ChordPlayer.getStaticStringHeight(stringId);
+    $guitar.find(`#strings .string.static.string-${stringId}`).css({height: `${staticStringHeight}%`});
+    $guitar.find(`#strings .string.active.string-${stringId}`).css({top: `${staticStringHeight}%`, height: `${100 - staticStringHeight}%`});
+    ChordPlayer.pluckString(stringId);
   },
   
   playChord(delay=20) {
-    var self = this;
-    for(var i = 0; i < this.strings.length; i++)
-      setTimeout(()=>self.play(i), delay * i);
+    for(let i = 0; i < ChordPlayer.state.strings.length; i++){
+      let stringId = i;
+      setTimeout(()=>ChordPlayer.pluckString(stringId), delay * i);
+    }
+  },
+  
+  pluckString(stringId) {
+    let $activeString = $(`#strings .string.active.string-${stringId}`);
+    let audioEl = $(`#audio #audio-${stringId}`)[0];
+    if(!audioEl)
+      return;
+    audioEl.pause();
+    if(ChordPlayer.state.frets[stringId] !== null) {
+      $activeString.addClass('animated').data('start', Date.now());
+      audioEl.currentTime = 0;
+      audioEl.play();
+    }
+    else {
+      $activeString.data('start', 0);
+    }
+    ChordPlayer.animateFrame();
+  },
+  
+  setAudio(stringId) {
+    let fretId = ChordPlayer.state.frets[stringId] || 0;
+    let best = '';
+    let diff = 999; //number of frets to get from plucked fret to recorded fret
+    let pitch = ChordPlayer.state.strings[stringId].transpose(fretId);
+    let audioClips = AUDIO_MAP[pitch.getName()];
+    for(let i = 0; i < audioClips.length; i++) {
+      let audioClip = audioClips[i];
+      let tstDiff = audioClip.fret - fretId;
+      if(Math.abs(tstDiff) < Math.abs(diff) || (Math.abs(tstDiff) === Math.abs(diff) && tstDiff < diff)) {
+        diff = tstDiff;
+        best = audioClip.file;
+      }
+    }
+    
+    let $audio = $(`#audio #audio-${stringId}`);
+    if($audio.length === 0) {
+      $audio = $(`<audio id="audio-${stringId}" preload />`);
+      $('#audio').append($audio);
+    }
+    $audio[0].pause();
+    $audio.attr('src', `audio/${best}.mp3`);
+  },
+  
+  /**
+   * Returns the height of the static portion of string ${i} as a percentage of window height, in 0-100 range.
+   */
+  getStaticStringHeight(stringId) {
+    let fretId = ChordPlayer.state.frets[stringId] || 0; //id of fingered fret on this string (0 for headstock)
+    let $fret = $('#guitar').find(fretId == 0 ? '#headstock' : `#fretboard .fret.fret-${fretId}`);
+    return 100 * ($fret.offset().top + $fret.outerHeight()) / window.innerHeight;
+  },
+  
+  animateFrame() {
+    const MAX_AMPLITUDE = 9;
+    const MAX_TIME = 2 * 1000;
+    const NOW = Date.now();
+    const FREQ = 20 * (2 * Math.PI / 1000);
+    let $animatedPaths = $('#strings .string.animated');
+    $animatedPaths.each(function() {
+      let $this = $(this);
+      let start = $this.data('start') || 0;
+      let t = NOW - start;
+      let amplitude = MAX_AMPLITUDE * Math.max(0, 1 - t/MAX_TIME);
+      let value = 10 + amplitude * Math.cos(FREQ * t);
+      
+      $this.find('svg path:first').attr('d', `M10 0 C ${value} 500, ${value} 500, ${value} 1000`);
+      if(amplitude <= 0)
+        $this.removeClass('animated');
+    });
+    
+    if($animatedPaths.length > 0)
+      requestAnimationFrame(ChordPlayer.animateFrame);
   }
 }
 
